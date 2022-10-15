511e2a3f5c1ef3fd07c98060ec8477ff22cb3450
zhgkj <zhgkaijun@gmail.com>
2022-10-14 22:15:41 -0400

    Initial commit.

---
+++ client/src/components/create.js
@@ -0,0 +1,77 @@
+import React, { useState } from "react";
+import { useNavigate } from "react-router";
+ 
+export default function Create() {
+ const [form, setForm] = useState({
+   name: "",
+   status: "",
+   level: "",
+ });
+ const navigate = useNavigate();
+ 
+ // These methods will update the state properties.
+ function updateForm(value) {
+   return setForm((prev) => {
+     return { ...prev, ...value };
+   });
+ }
+ 
+ // This function will handle the submission.
+ async function onSubmit(e) {
+   e.preventDefault();
+ 
+   // When a post request is sent to the create url, a new record is added to the database.
+   const newPerson = { ...form };
+ 
+   await fetch("http://localhost:5000/record/add", {
+     method: "POST",
+     headers: {
+       "Content-Type": "application/json",
+     },
+     body: JSON.stringify(newPerson),
+   })
+   .catch(error => {
+     window.alert(error);
+     return;
+   });
+ 
+   setForm({ name: "", status: "", level: "" });
+   navigate("/");
+ }
+ 
+ // This following section will display the form that takes the input from the user.
+ return (
+   <div>
+     <h3>Create New User</h3>
+     <form onSubmit={onSubmit}>
+       <div className="form-group">
+         <label htmlFor="name">Name</label>
+         <input
+           type="text"
+           className="form-control"
+           id="name"
+           value={form.name}
+           onChange={(e) => updateForm({ name: e.target.value })}
+         />
+       </div>
+       <div className="form-group">
+         <label htmlFor="status">Status</label>
+         <input
+           type="text"
+           className="form-control"
+           id="status"
+           value={form.status}
+           onChange={(e) => updateForm({ status: e.target.value })}
+         />
+       </div>
+       <div className="form-group">
+         <input
+           type="submit"
+           value="Create User"
+           className="btn btn-primary"
+         />
+       </div>
+     </form>
+   </div>
+ );
+}
