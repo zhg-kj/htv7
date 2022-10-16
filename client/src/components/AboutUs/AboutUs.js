169d20df103bbd8154d6afdc037e1601ca73f2d0
jacksonsujh <72714023+jacksonsujh@users.noreply.github.com>
2022-10-15 21:03:02 -0400

    Added web intro with used tech

--- client/src/components/AboutUs/AboutUs.js
+++ client/src/components/AboutUs/AboutUs.js
@@ -1,12 +1,15 @@
 import React from 'react';
 
 import "./AboutUs.css"
+import webIntroPic from "../../Assets/WebIntro.png";
 
 export class AboutUs extends React.Component{
     render() {
         return(
-            <div>
-
+            <div className="usingTech">
+                <img src={webIntroPic} alt="Company Intro"/>
+                <h1>{this.props.title}</h1>
+                <p>{this.props.intro}</p>
             </div>
         );
     }
