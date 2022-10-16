96540746b5dc086a5ca68c397fed33bc82d2dc0f
zhgkj <zhgkaijun@gmail.com>
2022-10-16 07:03:06 -0400

    Finishing touches.

--- client/src/pages/creator.js
+++ client/src/pages/creator.js
@@ -52,18 +52,39 @@ function Creator(props) {
 
     <div className="name2">
       <h4 className="title">David Lin</h4>
-      <h5 className="bio">Hi, my name is David Lin  </h5>
+      <h5 className="bio">Hi, my name is David Lin!</h5>
       <hr></hr>
-      <h6 className="sub">Choose a Membership Level!</h6>
     </div>
 
+    <div>
+      {!isCoinHolder ? 
+      <div>
+        <div className='bio3'>
+          <p className='bio-header'> ABOUT ME</p>
+          <p className='bio-content'> "My name is David Lin. I love to draw. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."</p>
+        </div>
+        <div style={{position: "absolute", top: "1000px"}}>
+          <ImageGallery/>
+        </div>
+      </div> : 
+      <><div className='bio2'>
+        <p className='bio-header'> ABOUT ME</p>
+        <p className='bio-content'> My name is David Lin, a second year student at the unviersty of Toronto,
+        My name is David Lin, a second year student at the unviersty of Toronto
+        My name is David Lin, a second year student at the unviersty of Toronto
+        My name is David Lin, a second year student at the unviersty of Toronto
+        My name is David Lin, a second year student at the unviersty of Toronto
+        My name is David Lin, a second year student at the unviersty of Toronto
+        My name is David Lin, a second year student at the unviersty of Toronto
+        My name is David Lin, a second year student at the unviersty of Toronto</p>
+    </div>
     <div class='float-container'>
       <div class='float-child'>
-        <h7 className="t1">TIER 1</h7>
+        <p className="t1">TIER 1</p>
         <hr className='line'></hr>
-        <h7 className="t1">25 COINS</h7>
-        <hr className='line'></hr>
-        <h11 className='t1'>About this Tier</h11>
+        <p className="t1">25 COINS</p>
+        <p className='line'></p>
+        <p className='t1'>About this Tier</p>
         <hr className='line'></hr>
         <div className=' button'>
           <Button />
@@ -71,11 +92,11 @@ function Creator(props) {
       </div>
 
       <div class='float-child'>
-        <h8 className="t2">TIER 2</h8>
+        <p className="t2">TIER 2</p>
         <hr className='line'></hr>
-        <h8 className="t2">50 COINS</h8>
+        <p className="t2">50 COINS</p>
         <hr className='line'></hr>
-        <h11 className='t2'>About this Tier</h11>
+        <p className='t2'>About this Tier</p>
         <hr className='line'></hr>
         <div className=' button'>
           <Button />
@@ -83,32 +104,19 @@ function Creator(props) {
       </div>
       
       <div class='float-child'>
-        <h9 className="t3">TIER 3</h9>
+        <p className="t3">TIER 3</p>
         <hr className='line'></hr>
-        <h9 className="t1">100 COINS</h9>
+        <p className="t1">100 COINS</p>
         <hr className='line'></hr>
-        <h11 className='t3'>About this Tier</h11>
+        <p className='t3'>About this Tier</p>
         <hr className='line'></hr>
         <div className=' button'>
           <Button />
         </div>
       </div>
     </div>
-
-    <div className='bio2'>
-        <h10 className='bio-header'> ABOUT ME</h10>
-        <h11 className='bio-content'> My name is David Lin, a second year student at the unviersty of Toronto,
-        My name is David Lin, a second year student at the unviersty of Toronto
-        My name is David Lin, a second year student at the unviersty of Toronto
-        My name is David Lin, a second year student at the unviersty of Toronto
-        My name is David Lin, a second year student at the unviersty of Toronto
-        My name is David Lin, a second year student at the unviersty of Toronto
-        My name is David Lin, a second year student at the unviersty of Toronto
-        My name is David Lin, a second year student at the unviersty of Toronto</h11>
+    </>}
     </div>
-
-
-    {isCoinHolder && <ImageGallery/>}
   </div>
   );
 }
